90908c7c5961abfc29910ef4542c8e7b4d75e0d2
kevin <kevin.fsilvas@gmail.com>
2025-11-27 19:58:22 -0300

    Create Cadastrar-Medico.php

---
+++ Cadastrar-Medico.php
@@ -0,0 +1,22 @@
+<h1>Cadastrar Médico</h1>
+<form action="?page=salvar-medico" method="POST">
+    <input type="hidden" name="acao" value="cadastrar">
+    <div class="mb-3">
+        <label>Nome do Médico</label>
+        <input type="text" name="nome_medico" class="form-control">
+    </div>
+
+    <div class="mb-3">
+        <label>CRM</label>
+        <input type="text" name="crm_medico" class="form-control">
+    </div>
+
+    <div class="mb-3">
+        <label>Especialidade</label>
+        <input type="text" name="especialidade_medico" class="form-control">
+    </div>
+    <div class="mb-3">
+        <button class="btn btn-success" type="submit">Salvar</button>
+    </div>
+
+</form>
