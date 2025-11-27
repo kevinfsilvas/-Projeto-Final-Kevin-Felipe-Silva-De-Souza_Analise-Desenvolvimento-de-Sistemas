b9e3e4432c59fc5f142af28117ba6e20b031dd0a
kevin <kevin.fsilvas@gmail.com>
2025-11-27 20:15:57 -0300

    Create Editar-Médico.php

---
+++ Editar-Médico.php
@@ -0,0 +1,32 @@
+<h1>Editar Médico</h1>
+<?php
+$sql ="SELECT * FROM medico WHERE id_medico=".$_REQUEST['id_medico'];
+
+$res = $conn->query($sql);
+
+$row = $res->fetch_object();
+
+?>
+
+<form action="?page=salvar-medico" method="POST">
+    <input type="hidden" name="acao" value="editar">
+    <input type="hidden" name="id_medico" value="<?php echo $row->id_medico; ?>">
+    <div class="mb-3">
+        <label>Nome do Médico</label>
+        <input type="text" name="nome_medico" class="form-control" value="<?php echo $row->nome_medico;?>">
+    </div>
+
+    <div class="mb-3">
+        <label>CRM</label>
+        <input type="text" name="crm_medico" class="form-control" value="<?php echo $row->crm_medico;?>">
+    </div>
+
+    <div class="mb-3">
+        <label>Especialidade</label>
+        <input type="text" name="especialidade_medico" class="form-control"value="<?php echo $row->especialidade_medico;?>">
+    </div>
+    <div class="mb-3">
+        <button class="btn btn-success" type="submit">Enviar</button>
+    </div>
+
+</form>
